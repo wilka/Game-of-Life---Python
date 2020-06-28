fe1dfa24caafb3a5b459bfa8657bf64135eedf40
Wilka Hudson <wilka.hudson@gmail.com>
2020-06-28 20:22:54 +0100

    Added a unit test
    You can run the test file with `py -m unittest test_gameBoard.py`

---
+++ test_gameBoard.py
@@ -0,0 +1,52 @@
+import unittest
+from gameBoard import GameBoard
+from point import Point
+
+# You can run this test file with `py -m unittest test_gameBoard.py`
+
+class TestGameBoard(unittest.TestCase):
+    def test_blinker_oscillator(self):
+        # https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life#Examples_of_patterns
+        # Blinker is an oscillator with period 2
+
+        board = GameBoard((10, 10))
+
+        # Make a blinker starting vertical
+        # -X-
+        # -X-
+        # -X-
+
+        board.set_cell(Point(4, 1), True)
+        board.set_cell(Point(4, 2), True)
+        board.set_cell(Point(4, 3), True)
+
+        # Move to next generation
+        board = board.next_generation()
+
+        # The blinker should switch to horizontal
+        # ---
+        # XXX
+        # ---
+        self.assertFalse(board.is_cell_alive(Point(4, 1)))
+        self.assertFalse(board.is_cell_alive(Point(4, 3)))
+
+        self.assertTrue(board.is_cell_alive(Point(3, 2)))
+        self.assertTrue(board.is_cell_alive(Point(4, 2)))
+        self.assertTrue(board.is_cell_alive(Point(5, 2)))
+
+
+        # Move to next generation again
+        board = board.next_generation()
+
+        # The blinker should switch back to vertical
+        # -X-
+        # -X-
+        # -X-
+        self.assertTrue(board.is_cell_alive(Point(4, 1)))
+        self.assertTrue(board.is_cell_alive(Point(4, 2)))
+        self.assertTrue(board.is_cell_alive(Point(4, 3)))
+
+        self.assertFalse(board.is_cell_alive(Point(3, 2)))
+        self.assertFalse(board.is_cell_alive(Point(5, 2)))        
+
+        
